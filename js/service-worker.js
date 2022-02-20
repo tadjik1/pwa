acdf1b1ff5fd96814260def20bcc360a0d5bb5cc
Sergey Zelenov <zelenov@so1.ai>
2022-02-20 21:07:04 +0100

    up

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,7 +1,18 @@
 const cacheName = 'pwa-v1';
 
 const contentToCache = [
-
+  '/pwa/index.html',
+  '/pwa/css/styles.css',
+  '/pwa/js/knockout-3.5.1.js',
+  '/pwa/js/localforge-1.10.0.js',
+  '/pwa/js/application.js',
+  '/pwa/js/main-screen.js',
+  '/pwa/images/android-chrome-192x192.png',
+  '/pwa/images/android-chrome-512x512.png',
+  '/pwa/images/apple-touch-icon.png',
+  '/pwa/images/favicon.ico',
+  '/pwa/images/favicon-16x16.png',
+  '/pwa/images/favicon-32x32.png',
 ];
 
 self.addEventListener('install', (e) => {
